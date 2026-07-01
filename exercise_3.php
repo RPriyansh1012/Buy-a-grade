97bd57e9ce5041e011787c967849eaaeb2f99608
Priyansh_R <priyanshratnala@gmail.com>
2026-07-01 16:44:15 -0400

    Debugged sucker.php and permissions so that it works in Codd

--- exercise_3.php
+++ exercise_3.php
@@ -1,8 +1,34 @@
 <h1>Raw Form Data</h1>
-<pre><?php print_r($_POST); ?></pre>
+
+<pre>
+
+<?php
+
+print_r($_POST);
+
+?>
+
+</pre>
+
 
 <h1>Form input values</h1>
-<p>Your Name: <?= htmlspecialchars($_POST['name'] ?? '') ?></p>
-<p>Section: <?= htmlspecialchars($_POST['section'] ?? '') ?></p>
-<p>Card Number: <?= htmlspecialchars($_POST['cardnumber'] ?? '') ?></p>
-<p>Card Type: <?= htmlspecialchars($_POST['cardtype'] ?? '') ?></p>
+
+
+<p>Your Name:
+<?= htmlspecialchars($_POST['name'] ?? '') ?>
+</p>
+
+
+<p>Section:
+<?= htmlspecialchars($_POST['section'] ?? '') ?>
+</p>
+
+
+<p>Card Number:
+<?= htmlspecialchars($_POST['cardnumber'] ?? '') ?>
+</p>
+
+
+<p>Card Type:
+<?= htmlspecialchars($_POST['cardtype'] ?? '') ?>
+</p>
--- sucker.php
+++ sucker.php
@@ -1,46 +1,13 @@
 <?php
-
-$required = ['name', 'section', 'cardnumber', 'cardtype'];
-
-foreach ($required as $field)
-{
-    if (!isset($_POST[$field]) || (is_array($_POST[$field]) && empty($_POST[$field])))
-    {
-        echo '<h1>Sorry</h1>';
-        echo '<p>You did not fill out the form completely. <a href="buyagrade.html">Try again?</a></p>';
-        exit;
-    }
-}
-
-
-$name = trim($_POST['name']);
-
-$section = $_POST['section'];
-
-if (is_array($section))
-{
-    $section = $section[0];
-}
-
-$section = trim($section);
-
-
-$cardnumber = trim($_POST['cardnumber']);
-
-$cardtype = trim($_POST['cardtype']);
-
+$name = trim($_POST['name'] ?? '');
+$section = trim($_POST['section'] ?? '');
+$cardnumber = trim($_POST['cardnumber'] ?? '');
+$cardtype = trim($_POST['cardtype'] ?? '');
 
 $line = $name . ';' . $section . ';' . $cardnumber . ';' . $cardtype . PHP_EOL;
-
-
 file_put_contents('suckers.html', $line, FILE_APPEND);
 
-
 $all = file_get_contents('suckers.html');
-
-
 echo '<h2>The current database contains:</h2>';
-
 echo '<pre>' . htmlspecialchars($all) . '</pre>';
-
 ?>
