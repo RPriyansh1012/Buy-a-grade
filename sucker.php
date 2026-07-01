6a1b6fcbabb5c94b00e7ce03375f996b02b0fcec
Priyansh_R <priyanshratnala@gmail.com>
2026-07-01 17:14:36 -0400

    Debugged the validation error

--- sucker.php
+++ sucker.php
@@ -1,13 +1,35 @@
 <?php
-$name = trim($_POST['name'] ?? '');
-$section = trim($_POST['section'] ?? '');
-$cardnumber = trim($_POST['cardnumber'] ?? '');
-$cardtype = trim($_POST['cardtype'] ?? '');
+
+$required = ['name', 'section', 'cardnumber', 'cardtype'];
+
+foreach ($required as $field)
+{
+    if (!isset($_POST[$field]) || trim($_POST[$field]) === '')
+    {
+        echo '<h1>Sorry</h1>';
+        echo '<p>You did not fill out the form completely. <a href="buyagrade.html">Try again?</a></p>';
+        exit;
+    }
+}
+
+
+$name = trim($_POST['name']);
+$section = trim($_POST['section']);
+$cardnumber = trim($_POST['cardnumber']);
+$cardtype = trim($_POST['cardtype']);
+
 
 $line = $name . ';' . $section . ';' . $cardnumber . ';' . $cardtype . PHP_EOL;
+
+
 file_put_contents('suckers.html', $line, FILE_APPEND);
 
+
 $all = file_get_contents('suckers.html');
+
+
 echo '<h2>The current database contains:</h2>';
+
 echo '<pre>' . htmlspecialchars($all) . '</pre>';
+
 ?>
